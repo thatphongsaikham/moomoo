fbdc730d83c181eed2ae7b5afde08157c9f1c792
Tikka <natthakan.tha@dome.tu.ac.th>
2025-11-29 19:38:50 +0700

    Add menuItems data for beverage options

---
+++ frontend/src/menuData.js
@@ -0,0 +1,26 @@
+export const menuItems = [
+  {
+    id: 1,
+    name: "Latte",
+    price: 65,
+    isSpecial: false
+  },
+  {
+    id: 2,
+    name: "Cappuccino",
+    price: 70,
+    isSpecial: false
+  },
+  {
+    id: 3,
+    name: "Strawberry Latte",
+    price: 85,
+    isSpecial: true
+  },
+  {
+    id: 4,
+    name: "Matcha Frappe",
+    price: 90,
+    isSpecial: true
+  }
+];
